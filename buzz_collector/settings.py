f3ee6d43dd91cfaaf5ce475b56e5af13c1af2653
Dor <dorzairi@ymail.com>
2021-04-01 20:52:33 -0400

    Added gitignore, changed settings and tweaked models

--- buzz_collector/settings.py
+++ buzz_collector/settings.py
@@ -11,9 +11,15 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Env 
+environ.Env()
+environ.Env.read_env()
 
 
 # Quick-start development settings - unsuitable for production
--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+import os
 
+def some_function(request):
+    my_key = os.environ['SECRET_KEY']
+    
 # Create your models here.
