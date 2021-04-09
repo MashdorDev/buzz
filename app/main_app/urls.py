e1ef3f2d8b5696279e61a946b3132c3f21ae9337
raymond <raycasgrain@gmail.com>
2021-04-09 00:33:02 -0400

    last update from Ray

--- app/main_app/urls.py
+++ app/main_app/urls.py
@@ -9,12 +9,14 @@ urlpatterns = [
     path('', views.home, name = 'home'),
     path('accounts/signup/', views.signup, name= 'signup'),
     path('about/us/', views.about_view, name='about_us'),
+    
     # details and add pages
     path('store/<str:sto_id>/', views.store_details, name='store_details'),
     path('coffee/detail/<int:coff_id>/', views.coffee_detail, name='coffee_detail'),
     path('coffee/create/<str:sto_id>/', views.coffee_create, name='coffee_create'),
     path('addreview/<int:coff_id>/', views.create_review, name='review_create'),
     path('addfavorite/<int:coff_id>/', views.add_favorite, print('test'), name="add_favorite"),
+    
     # search page and cattegories
     path('search/', views.search, name='search'),
     path('searching/', views.searching, name='searching'),
@@ -24,6 +26,7 @@ urlpatterns = [
     path('type/ice', views.index_type_ice, name='type_ice'),
     path('type/esp', views.index_type_esp, name='type_esp'),
     path('type/cap', views.index_type_cap, name='type_cap'),
+    
     # profile related links
     path('profile/', views.profile, name='profile'),
     path('approval/admin', views.admin_approval, name='admin_approval'),
